d4624884d71e0edb2fbd5719db8c870849e3047f
Henrique Costa <henriquefcosta19@gmail.com>
2023-08-09 17:16:04 -0300

    fixes for deploy

--- app.ts
+++ app.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import dotEnv from "dotenv";
 import routes from "./src/routes";
-// import cors from "cors";
+import cors from "cors";
 import prisma from "./src/db/dbConnect";
 
 dotEnv.config();
 
 const app = express();
 const port = process.env.PORT;
-const host = "0.0.0.0";
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 routes(app);
 
 app.listen(port, () => {
