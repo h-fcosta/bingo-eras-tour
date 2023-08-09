38f4eb594587131ec7d7b330471dbc50da235e9b
Henrique Costa <henriquefcosta19@gmail.com>
2023-08-09 13:46:37 -0300

    preparing files for deploy

--- src/controllers/SongController.ts
+++ src/controllers/SongController.ts
@@ -1,80 +1,7 @@
 import { Response, Request } from "express";
-import redisClient from "../db/redis";
-import api from "../api";
 import prisma from "../db/dbConnect";
 
 export default class SongController {
-  static async fetchAndStoreAlbumTracks(req: Request, res: Response) {
-    const albumId = req.params.albumId;
-
-    try {
-      const tracks = await SongController.fetchAlbumTracksFromSpotify(albumId);
-      await SongController.storeTracksInDatabase(tracks.items, albumId);
-
-      return res.json(tracks.items);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  }
-
-  static async fetchAlbumTracksFromSpotify(albumId: string) {
-    try {
-      const accessToken = await redisClient.get("accessToken");
-
-      const response = await api.get(
-        `/albums/${albumId}/tracks?market=BR&limit=50`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`
-          }
-        }
-      );
-
-      return response.data;
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  static async storeTracksInDatabase(tracks: any[], albumId: string) {
-    if (!tracks || tracks.length === 0) {
-      console.log("No songs to store");
-      return;
-    }
-    const albumExists = await prisma.album.findUnique({
-      where: { spotify_album_id: albumId }
-    });
-
-    if (!albumExists) {
-      throw new Error("Album not found!");
-    }
-
-    for (const trackInfo of tracks) {
-      try {
-        const track = await prisma.song.create({
-          data: {
-            spotify_song_id: trackInfo.id,
-            song_name: trackInfo.name,
-            spotify_link: trackInfo.external_urls.spotify,
-            albumId: albumId
-          }
-        });
-
-        console.log("Track stored:", track);
-      } catch (error) {
-        if (
-          error.code === "P2002" &&
-          error.meta.target.includes("spotify_album_id")
-        ) {
-          console.log("Duplicate album ignored:", trackInfo.name);
-        } else {
-          console.error("Error:", error);
-        }
-      }
-    }
-  }
-
   static async getSongsAndSingles(req: Request, res: Response) {
     try {
       const songs = await prisma.song.findMany({
@@ -114,10 +41,6 @@ export default class SongController {
       );
 
       return res.json(songsAndSingles);
-      // const songsAndSingles =
-      //   await prisma.$queryRaw`SELECT * FROM song UNION SELECT * FROM single ORDER BY albumId;`;
-
-      // return res.json(songsAndSingles);
     } catch (error) {
       console.error(error);
 
--- src/routes/spotifyRoutes.ts
+++ src/routes/spotifyRoutes.ts
@@ -1,16 +1,8 @@
 import express from "express";
-import SpotifyController from "../controllers/SpotifyController";
-import AlbumController from "../controllers/AlbumController";
 import SongController from "../controllers/SongController";
-import SingleController from "../controllers/SingleController";
 
 const router = express.Router();
 
-router
-  .get("/token", SpotifyController.getAccessToken)
-  .get("/spotify/albums/:artistId", AlbumController.fetchAndStoreAlbum)
-  .get("/spotify/songs/:albumId", SongController.fetchAndStoreAlbumTracks)
-  .get("/songs", SongController.getSongsAndSingles)
-  .get("/spotify/singles/:artistId", SingleController.fetchAndStoreSingle);
+router.get("/songs", SongController.getSongsAndSingles);
 
 export default router;
