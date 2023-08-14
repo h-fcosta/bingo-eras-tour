88d1322e930d9815c7c1a83fb6224cc9845d5db4
Henrique Costa <henriquefcosta19@gmail.com>
2023-08-14 19:30:00 -0300

    db change to PostgreSQL

--- prisma/migrations/20230727004933_type_change/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `release_order` on the `Album` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-
-*/
--- AlterTable
-ALTER TABLE `Album` MODIFY `release_order` INTEGER NULL;
--- prisma/migrations/20230727011313_update_key_null/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Single` DROP FOREIGN KEY `Single_albumId_fkey`;
-
--- AlterTable
-ALTER TABLE `Single` MODIFY `albumId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Single` ADD CONSTRAINT `Single_albumId_fkey` FOREIGN KEY (`albumId`) REFERENCES `Album`(`spotify_album_id`) ON DELETE SET NULL ON UPDATE CASCADE;
