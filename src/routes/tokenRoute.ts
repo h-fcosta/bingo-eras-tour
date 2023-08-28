d4a3b4eaf7e4c3ac06bc2f8c5ac9e10fc858eaeb
Henrique Costa <henriquefcosta19@gmail.com>
2023-08-28 14:47:58 -0300

    The Eras Tour Surprise Song Bingo - deploy v1.2

--- src/routes/tokenRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import AuthController from "../controllers/AuthController";
-
-const router = express.Router();
-
-router.get("/token", AuthController.getAccessToken);
-
-export default router;
